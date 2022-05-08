3638b61efb56c81123c4e5b253eea0fee273ce17
Bin Yang <bin.yang@windriver.com>
2022-05-08 12:52:44 +0800

    update stress-cpu-test
    Signed-off-by: Bin Yang <bin.yang@windriver.com>

--- stress-cpu-test.sh
+++ stress-cpu-test.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-# cmd=""
-# watch=0
-hostname="controller-0"
+#hostname="controller-0"
 
 function prepare()
 {
-mkdir -p ~/testsuite
+    mkdir -p ~/testsuite
 # For AIO SX only, need update with affinity to controller-0?
 cat <<EOF > ~/testsuite/stress-cpu.yaml
 apiVersion: v1
@@ -21,38 +19,100 @@ spec:
     command: ["/stress-ng"]
     args: ["-c 0 -l 96"]
 EOF
-
 }
 
 function start()
 {
-prepare
-kubectl apply -f ~/testsuite/stress-cpu.yaml -n kube-system
+    export KUBECONFIG="/etc/kubernetes/admin.conf"
+    kubectl -n kube-system get pod stress-ng-cpu
+    if [ "$?" == 1 ]; then
+        prepare
+        kubectl apply -f ~/testsuite/stress-cpu.yaml -n kube-system
+        if [ "$?" == 0 ]; then
+        echo "status:pass"
+        else
+        echo "status:fail"
+        fi
+    else
+        echo "status:pass"
+    fi
 }
 
 function stop()
 {
-kubectl delete -f ~/testsuite/stress-cpu.yaml -n kube-system
+    export KUBECONFIG="/etc/kubernetes/admin.conf"
+    kubectl -n kube-system get pod stress-ng-cpu
+    if [ "$?" == 0 ]; then
+        kubectl delete -f ~/testsuite/stress-cpu.yaml -n kube-system
+        if [ "$?" == 0 ]; then
+        echo "status:pass"
+        else
+        echo "status:fail"
+        fi
+    else
+        echo "status:pass"
+    fi
+}
+
+function started()
+{
+    export KUBECONFIG="/etc/kubernetes/admin.conf"
+    kubectl -n kube-system get pod stress-ng-cpu
+    if [ "$?" == 0 ]; then
+        echo "status:pass"
+    else
+        echo "status:fail"
+    fi
+}
+
+function stopped()
+{
+    export KUBECONFIG="/etc/kubernetes/admin.conf"
+    kubectl -n kube-system get pod stress-ng-cpu
+    if [ "$?" == 1 ]; then
+        echo "status:pass"
+    else
+        echo "status:fail"
+    fi
 }
 
 function status()
 {
-kubectl -n kube-system get pod stress-ng-cpu -o wide
-kubectl -n kube-system describe pod stress-ng-cpu
-source /etc/platform/openrc
-fm alarm-list
+    export KUBECONFIG="/etc/kubernetes/admin.conf"
+    kubectl -n kube-system get pod stress-ng-cpu
+    if [ "$?" == 0 ]; then
+        kubectl -n kube-system get pod stress-ng-cpu -o wide
+        kubectl -n kube-system describe pod stress-ng-cpu
+        source /etc/platform/openrc
+        fm alarm-list
+        echo "status:pass"
+    else
+        echo "status:fail"
+    fi
 }
 
 function asserted()
 {
-source /etc/platform/openrc
-# fm alarm-list | grep "host=${hostname}" | grep "Platform CPU threshold exceeded"
-fm alarm-list | grep "Platform CPU threshold exceeded"
+    export KUBECONFIG="/etc/kubernetes/admin.conf"
+    kubectl -n kube-system get pod stress-ng-cpu
+    if [ "$?" == 0 ]; then
+        source /etc/platform/openrc
+        # fm alarm-list | grep "host=${hostname}" | grep "Platform CPU threshold exceeded"
+        fm alarm-list | grep "Platform CPU threshold exceeded"
+        if [ "$?" == 0 ]; then
+        echo "status:pass"
+        else
+        echo "status:fail"
+        fi
+    else
+        echo "status:pending"
+    fi
+
 }
 
 function usage()
 {
-echo "usage: stress-cpu-test.sh [start|stop|status|asserted]"
+    echo "usage: stress-cpu-test.sh [start|stop|status|asserted]"
 }
 
 while [ "$1" != "" ]; do
@@ -61,6 +121,12 @@ while [ "$1" != "" ]; do
                         exit
                         ;;
         stop )          stop
+                        exit
+                        ;;
+        started )       started
+                        exit
+                        ;;
+        stopped )       stopped
                         exit
                         ;;
         status )        status
